19e9d01fb5b057c8fc75c48a8510d6b3386d1c00
2019osbjoh <john.osborne151@gmail.com>
2019-11-05 00:12:20 +0000

    ~

---
+++ recusionNaturalNumbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int sum(int n);
+
+int main() {
+	int number, result;
+
+	printf("Enter a positive Int: ");
+	scanf("%d", &number);
+
+	result = sum(number);
+
+	printf("sum = %d", result);
+	printf("\n");
+	return 0; 
+}
+
+int sum(int num) {
+	if (num != 0)
+		return num + sum(num - 1);
+	else
+		return num;
+}
