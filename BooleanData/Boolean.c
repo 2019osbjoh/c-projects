2e9c5c015daf9291f7641d0c4b56a9113af0c9fd
2019osbjoh <john.osborne151@gmail.com>
2019-11-05 18:59:01 +0000

    ~

---
+++ BooleanData/Boolean.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	
+	_Bool ourBool = 1; // 0 == false 
+	
+	printf("True or False (1 = True | 0 = False): %i\n", ourBool);
+	
+	return 0;
+}
---
+++ BooleanData/StdBoolean.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	
+	bool ourBool = true; 
+	
+	printf("True or False (1 = True | 0 = False): %i\n", ourBool);
+	
+	
+	return 0;
+}
