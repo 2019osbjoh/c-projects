0968c120c7b70ea2224fa8063458ef9ea10421c8
2019osbjoh <john.osborne151@gmail.com>
2019-11-05 18:44:23 +0000

    ~

--- CharaterHandling/Char.c
+++ CharaterHandling/Char.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
+ 	
+	char ASCII;
+	int integer;	
+
+	printf("\nPlease enter a character: ");
+	scanf("%c", &ASCII);
+	printf("The integer value of that ASCII character is %i\n", ASCII);
 	
+	printf("\n");
+	printf("----------------------------------------------------------");
+	printf("\n");
+	
+	printf("\nPlease enter an intger between 0 - 127: ");
+	scanf("%i", &integer);
+	printf("The character is: %c\n\n", integer);
+		
+	printf("\n");
+	printf("----------------------------------------------------------");
+	printf("\n");
+
+	char maths = 'A' + '\t';
+	printf("\nA(65) + \\t(11) = %c(%d)\n", maths, maths);
+
 	return 0;
 }
