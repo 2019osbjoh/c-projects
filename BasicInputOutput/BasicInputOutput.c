97a90509507a7386e927c547ff1b3228b22ccdf4
2019osbjoh <john.osborne151@gmail.com>
2019-11-07 19:55:02 +0000

    ~

--- BasicInputOutput/BasicInputOutput.c
+++ BasicInputOutput/BasicInputOutput.c
@@ -7,12 +7,12 @@ int main() {
 	printf("\nEnter a radius to find the diameter: ");
 	scanf("%lf", &radius);
 			
-	diameter = radius / 2.0F;
-	area = 3.14159 * (radius * radius);
+	diameter = radius * 2.0F;
+	area = 3.141592653589793238462643383 * (radius * radius);
 
- 	printf("\nThe given radius is: %g\n", radius);
-	printf("The Diameter is: %g\n", diameter);	
-	printf("The area is equal to around: %g\n\n", area);
+ 	printf("\nThe given radius is: %f\n", radius);
+	printf("The Diameter is: %f\n", diameter);	
+	printf("The area is equal to around: %f\n\n", area);
 	
 	return 0;
 }
