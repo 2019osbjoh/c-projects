db21170b41fdd6e89b575931a25a2246a545c496
2019osbjoh <john.osborne151@gmail.com>
2019-11-25 19:32:55 +0000

    ~

--- Arrays/Arrays.c
+++ Arrays/Arrays.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 
+void display(int a[], int num) {
+	int x;
+
+	printf("\nnumbers: ");	
+
+	for (x = 0; x < num; x++) {
+		printf("%d ", a[x]);
+	}
+}
+
+
+void input(int a[], int num) {
+	int x;
+
+	for (x = 0; x < num; x++) {
+		printf("Enter an element: ");
+		scanf("%d", &a[x]);
+	}
+} 
+
+
 int main() {
 	// Calculates prime numbers by removing multiples interatively from a list
+	int length = 10;
 	int i;
-	int a = 5;
-	int list[a];
+	int list[length];
 	
 	printf("\n");
-	for (i = 0; i < 5; i++) {
-		printf("Enter a number: ");	
-		scanf("%d", &list[i]);
-	}
-	
-	printf("\nNumbers: ");
-	for (i = 0; i < 5; i++) {
-		printf("%d ", list[i]);
-	}
-	printf("\n");	
+
+	input(list, length);
+	display(list, length);
+
+	printf("\n\n");	
 
 	return 0;
-	
 }
