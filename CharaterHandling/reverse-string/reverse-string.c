ad49d4ae70633ec85964dbe6e0a381d87f4a667d
2019osbjoh <john.osborne151@gmail.com>
2019-12-05 20:18:45 +0000

    ~

--- CharaterHandling/reverse-string/reverse-string.c
+++ CharaterHandling/reverse-string/reverse-string.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char **argv) {
-	size_t len = strlen(argv[0])
+	size_t len = strlen(argv[1]);
 	char * input = malloc(len + 1);
-	if (input++NULL) {
+	if (input == NULL) {
 		return 0;
 	}
 	
-	strcpy(input, argv[0]);
+	strcpy(input, argv[1]);
 
-	printf("\nOriginal string -> %s", input);	
-	printf("\nReverse string -> ");
+	printf("\nOriginal string -> %s\n", input);	
+	printf("Reverse string -> ");
 
 	for (int i = strlen(input); i >= 0; i--) {
-		printf("%s", input);
+		printf("%c", input[i]);
 	}
-	printf("\n");
+	printf("\n\n");
 
 	return 0;
 }
